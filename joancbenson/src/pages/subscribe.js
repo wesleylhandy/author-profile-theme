9924bc675ed47bb84c5b6edf3ef1d13710c649fe
Wesley L. Handy <wesleylhandy@gmail.com>
2021-02-06 02:07:59 -0500

    add subscribe page;

---
+++ joancbenson/src/pages/subscribe.js
@@ -0,0 +1,16 @@
+/** @jsx jsx */
+import Layout from '@wesleylhandy/gatsby-theme-author-base/src/components/layout'
+import Seo from '@wesleylhandy/gatsby-theme-author-base/src/components/seo'
+import MailchimpWidget from "../components/mailchimp-widget"
+
+const SubscribePage = ({ location }) => {
+  return (
+    <Layout location={location} hideSidebar={true}>
+      <Seo type="website" title="Subscribe Page" description={"Joan C. Benson regularly posts to her blog and will be promoting her writings and events via her newsletter. Subscribe here."}/>
+      <h1>Subscribe To My Newsletter</h1>
+      <MailchimpWidget heading={""} />
+    </Layout>
+  )
+}
+
+export default SubscribePage
