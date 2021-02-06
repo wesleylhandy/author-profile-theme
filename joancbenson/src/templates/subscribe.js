954650ea1593e2bee2d8028d552b928c42b18357
Wesley L. Handy <wesleylhandy@gmail.com>
2021-02-06 14:35:44 -0500

    subscribe page styling

--- joancbenson/src/templates/subscribe.js
+++ joancbenson/src/templates/subscribe.js
@@ -7,8 +7,10 @@ const SubscribePage = ({ location }) => {
   return (
     <Layout location={location} hideSidebar={true}>
       <Seo type="website" title="Subscribe Page" description={"Joan C. Benson regularly posts to her blog and will be promoting her writings and events via her newsletter. Subscribe here."}/>
-      <h1>Subscribe To My Newsletter</h1>
-      <MailchimpWidget heading={""} />
+      <section sx={{flex: "1 1 auto", display: "flex", flexDirection: ["column", "column", "row"]}}>
+        <h1>Subscribe To My Newsletter</h1>
+        <MailchimpWidget heading={""} />
+      </section>
     </Layout>
   )
 }
