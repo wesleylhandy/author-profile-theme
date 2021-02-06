151785eb28508987207911bc1a63dafce3a1166e
Wesley L. Handy <wesleylhandy@gmail.com>
2021-02-06 16:05:45 -0500

    update subscribe page title

--- joancbenson/src/templates/subscribe.js
+++ joancbenson/src/templates/subscribe.js
@@ -7,7 +7,7 @@ import MailchimpWidget from "../components/mailchimp-widget"
 const SubscribePage = ({ location }) => {
   return (
     <Layout location={location} hideSidebar={true}>
-      <Seo type="website" title="Subscribe Page" description={"Joan C. Benson regularly posts to her blog and will be promoting her writings and events via her newsletter. Subscribe here."}/>
+      <Seo type="website" title="Subscribe" description={"Joan C. Benson regularly posts to her blog and will be promoting her writings and events via her newsletter. Subscribe here."}/>
       <section sx={{flex: "1 1 auto"}}>
         <h1 sx={{textAlign: "center"}}>Subscribe To My Newsletter</h1>
         <MailchimpWidget heading={""} showBorders={false} />
